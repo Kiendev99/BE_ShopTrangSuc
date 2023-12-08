b3bf9516dae5d17cd03ac519f8a09220b805fd78
chien2k3 <106466627+chien2k3@users.noreply.github.com>
2023-12-09 04:11:49 +0700

    [BE] Fix routes brand

--- routes/brand.js
+++ routes/brand.js
@@ -6,6 +6,7 @@ const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 
 router.post('/add', [verifyAccessToken, isAdmin], ctrls.createdBrand)
 router.get('/', ctrls.getAllBrand)
+router.get('/:id', ctrls.getBrand)
 router.put('/:brid', [verifyAccessToken, isAdmin], ctrls.updateBrand)
 router.delete('/:brid', [verifyAccessToken, isAdmin], ctrls.deleteBrand)
 router.get('/brand/:brand', ctrls.getProductsByBrandId);
